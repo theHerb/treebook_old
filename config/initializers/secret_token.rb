fbea7f5f766d10fc97aae158bbd3ff1c66946407
Matt Hartwig <msh.cal.ab@gmail.com>
2012-10-18 15:07:36 -0600

    Revert "Created a Rails Applicatin and Added Status Updates"
    This reverts commit bb342c08b49bdca6be478514d8ca7223c2edfe0f.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Treebook::Application.config.secret_token = '1a7aeb5f6394d9eb04981f3be23fe5d510b5a86923c8f605a97f38faa9ad48457075f43f5a6bc82e0d28698e707cef8f57b12a2e6a8c3db995669a3ca29c2cda'
